6a2a50878bff5fbbe1258a7843ceeda646d122e3
jwjung1219 <jwjung1219@eotechnics.com>
2026-03-13 10:53:11 +0900

    초기 소스 코드 커밋

---
+++ A Project/Form1.cs
@@ -0,0 +1,15 @@
+namespace A_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
