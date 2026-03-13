588b16a1fd8d637217dd87c74528b01218dad5c6
jwjung1219 <jwjung1219@eotechnics.com>
2026-03-13 14:09:47 +0900

    VS Code 커밋 및 Push

--- A Project/Form1.cs
+++ A Project/Form1.cs
@@ -9,8 +9,7 @@ namespace A_Project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Commit test1 진행
-            //Commit test2 진행 - 터미널 메시지
+            //Commit test1 진행 커밋 + 푸시
         }
     }
 }
