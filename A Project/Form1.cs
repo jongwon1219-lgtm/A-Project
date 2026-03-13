a213ae3dca93daa17ec9f72f247c1960c13cf89b
jwjung1219 <jwjung1219@eotechnics.com>
2026-03-13 14:01:17 +0900

    Commit Test2 진행 터미널 메시지로

--- A Project/Form1.cs
+++ A Project/Form1.cs
@@ -10,7 +10,7 @@ namespace A_Project
         private void button1_Click(object sender, EventArgs e)
         {
             //Commit test1 진행
-
+            //Commit test2 진행 - 터미널 메시지
         }
     }
 }
